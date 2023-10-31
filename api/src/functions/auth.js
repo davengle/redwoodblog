034298933592325d164920252f0492fa0e8d609d
Dave Engle <davengle@gmail.com>
2023-10-31 17:16:57 -0400

    added second admin user

--- api/src/functions/auth.js
+++ api/src/functions/auth.js
@@ -103,14 +103,19 @@ export const handler = async (event, context) => {
     // If this returns anything else, it will be returned by the
     // `signUp()` function in the form of: `{ message: 'String here' }`.
     handler: ({ username, hashedPassword, salt, userAttributes }) => {
-      return db.user.create({
-        data: {
-          email: username,
-          hashedPassword: hashedPassword,
-          salt: salt,
-          // name: userAttributes.name
-        },
-      })
+      // Creating a user is normal operation but this would allow anyone accessing the site to create an admin user.
+      // return db.user.create({
+      //   data: {
+      //     email: username,
+      //     hashedPassword: hashedPassword,
+      //     salt: salt,
+      //     // name: userAttributes.name
+      //   },
+
+      //Returning false block hackers from creating new users
+        return false;
+      }
+      // )
     },
 
     // Include any format checks for password here. Return `true` if the
